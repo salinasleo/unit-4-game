a991e231a5c600f2349d65a546b115c16be8c260
Leo Salinas <salinasleo@gmail.com>
2018-06-02 01:12:43 -0400

    more

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+$("body").on("click", "#jelly1", function() {
+
+    alert("Mmm... tasty jelly 1");
+});
+
+
+$("body").on("click", "#jelly2", function() {
+
+    alert("Mmm... tasty jelly 2");
+});
+
+$("body").on("click", "#jelly3", function() {
+
+    alert("Mmm... tasty jelly 3");
+});
+
+$("body").on("click", "#jelly4", function() {
+
+    alert("Mmm... tasty jelly 4");
+});
